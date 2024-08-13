4a6e5c441c9c8da092e5781c39808c14d6da7d79
karthick <karthickpalanivel4@gmail.com>
2024-08-13 15:49:43 +0530

    added product details pages

--- client/data/ProductsData.js
+++ client/data/ProductsData.js
@@ -1,9 +1,10 @@
 export const ProductsData = [
   {
     _id: 1,
-    name: "Iphone 15",
-    description: "something looks luxury but except camera nothing good",
+    name: "Apple iPhone 15 pro plus (256 GB) grey shades",
+    description: "something looks luxury but except camera nothing good, manufactured from china, product by USA, delivered to you, where ever in this world",
     price: "300",
+    quantity: "100",
     category: "mobile",
     imageUrl: "https://m.media-amazon.com/images/I/71657TiFeHL._SX522_.jpg",
   },
--- client/src/components/products/ProductDetails.jsx
+++ client/src/components/products/ProductDetails.jsx
@@ -1,26 +1,114 @@
-import { StyleSheet, Text, View } from "react-native";
-import React, { useEffect, useState } from "react";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useState, useEffect } from "react";
+import { ProductsData } from "../../../data/ProductsData";
+import Layout from "../Layout/Layout";
+import Header from "../Layout/Header";
+import Categories from "../category/Categories";
 
 const ProductDetails = ({ route }) => {
-  const [productDisplay, setProductDisplay] = useState({});
-
-  const { params } = route;
+  const [pDetails, setPDetails] = useState({});
+  const [qty, setQty] = useState(1);
 
   useEffect(() => {
-    //find product
-    const getProduct = ProductDetails.find((p) => {
-      return p._id === params.id;
+    const getProduct = ProductsData.find((item) => {
+      return item?._id === params?._id;
     });
-    setProductDisplay(getProduct);
-  });
+    setPDetails(getProduct);
+  }, [route.params._id]);
+
+  const addQty = () => {
+    if (qty === 10) return alert(`you can't add more than 10 quantity`);
+    return setQty((prev) => prev + 1);
+  };
+  const subQty = () => {
+    if (qty <= 0) return;
+    setQty((prev) => prev - 1);
+  };
+
+  const { params } = route;
   return (
-    <View>
-      <Text>ProductDetails {params._id}</Text>
-      <Text>Product Details {JSON.stringify(p)}</Text>
+    <View style={{ backgroundColor: "white" }}>
+      <Header />
+      <Categories />
+      {/* <Text>ProductDetails {params._id}</Text>
+      <Text>{JSON.stringify(pDetails,null,4)}</Text> */}
+
+      <Image source={{ uri: pDetails?.imageUrl }} style={styles.image} />
+      <View style={styles.container}>
+        <Text style={styles.title}>{pDetails?.name}</Text>
+        <Text style={styles.title}>${pDetails.price}</Text>
+        <Text style={styles.desc}>{pDetails.description}</Text>
+      </View>
+
+      <View style={styles.btnContainer}>
+        <TouchableOpacity style={styles.btnCart}>
+          <Text style={{ color: "white" }}>Add to Cart</Text>
+        </TouchableOpacity>
+
+        <View style={styles.btnContainer}>
+          <TouchableOpacity style={styles.btnQty} onPress={qty >= 0 && subQty}>
+            <Text style={styles.btnQtyText}>-</Text>
+          </TouchableOpacity>
+          <Text>{qty}</Text>
+          <TouchableOpacity style={styles.btnQty} onPress={addQty}>
+            <Text style={styles.btnQtyText}>+</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
 
 export default ProductDetails;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 300,
+    width: "100%",
+  },
+  container: {
+    marginHorizontal: 10,
+    marginVertical: 15,
+  },
+  title: {
+    fontSize: 18,
+    textAlign: "left",
+  },
+  desc: {
+    fontSize: 14,
+    textTransform: "capitalize",
+    textAlign: "justify",
+    marginVertical: 20,
+  },
+  btnContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginHorizontal: 10,
+    marginVertical: 20,
+  },
+  btnCart: {
+    width: 180,
+    backgroundColor: "#000",
+    borderRadius: 5,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    marginHorizontal: 6,
+  },
+  btnCartText: {
+    color: "#fff",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 16,
+  },
+  btnQty: {
+    backgroundColor: "lightgray",
+    width: 40,
+    alignItems: "center",
+    marginHorizontal: 10,
+  },
+  btnQtyText: {
+    fontSize: 18,
+  },
+});
